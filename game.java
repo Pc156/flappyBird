21c9b0e60a01bd67a18557f5065ffd5db5134761
Pc156 <cprathmesh314159@gmail.com>
2024-06-01 08:26:34 +0530

    /

---
+++ game.java
@@ -0,0 +1,11 @@
+/**
+ * game
+ */
+public class game {
+
+    public static void main(String[] args) {
+        System.out.println("first flappy bird game");
+
+        System.out.println("built in js ");
+    }
+}
